6cc807ad90a8b8a08b913c4f03ba81329e63ad1d
Diego Silva <coxcorp@gmail.com>
2022-07-27 18:30:32 -0300

    requisito 02

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,6 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    elif low_index > high_index:
+        return True
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
