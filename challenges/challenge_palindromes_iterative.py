29d677f106ee5c5ad2e3c99708ebc7a904c44074
Diego Silva <coxcorp@gmail.com>
2022-07-28 16:16:17 -0300

    refatoração

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,7 +1,7 @@
 def is_palindrome_iterative(word):
-    if not word:
-        return False
-    lista = list(word)
-    invertida = lista.copy()
-    invertida.reverse()
-    return lista == invertida
+    if word:
+        lista = list(word)
+        invertida = lista.copy()
+        invertida.reverse()
+        return lista == invertida
+    return False
