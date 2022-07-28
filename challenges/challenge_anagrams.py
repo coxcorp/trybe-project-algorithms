35c7bb9d1f5e1dda25ba6ea335108a561496e288
Diego Silva <coxcorp@gmail.com>
2022-07-28 11:37:23 -0300

    requisito 03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,20 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+
+    first_list = list(first_string.upper())
+    second_list = list(second_string.upper())
+
+    letter_qtd = 0
+
+    for letter in first_list:
+        if letter in second_list:
+            letter_qtd += 1
+            second_list.remove(letter)
+        else:
+            return False
+
+    if letter_qtd == len(first_list):
+        return True
+
+    return False
