fb81b4883805730d042cd6c7544fb5f162db86d1
Diego Silva <coxcorp@gmail.com>
2022-07-28 16:09:04 -0300

    requisito 04

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,12 @@
+from collections import Counter
+
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if nums:
+        for num in nums:
+            if type(num) != int or num < 1:
+                return False
+        most = Counter(nums).most_common(1)[0]
+        if most[1] > 1:
+            return most[0]
+    return False
